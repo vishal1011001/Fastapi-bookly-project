4583e01875df31338e669e275184f5b2d701eaa4
vishal1011001 <vishaljakharbusinessmail@gmail.com>
2026-06-18 11:49:21 +0530

    Added user auth model. Installed Alembic Migrations.

---
+++ src/auth/models.py
@@ -0,0 +1,28 @@
+from sqlmodel import SQLModel, Field, Column
+import sqlalchemy.dialects.postgresql as pg
+import uuid
+from datetime import datetime, date
+
+class User(SQLModel, table=True):
+    __tablename__ = 'users'
+    
+    uid : uuid.UUID = Field(
+        sa_column = Column(
+            pg.uuid,
+            nullable=False,
+            primary_key=True,
+            default=uuid.uuid4
+        )
+    )
+    username: str
+    email: str
+    first_name: str
+    last_name: str
+    is_verified: bool = Field(sa_column=Column(pg.BOOLEAN, default=False))
+    password: str
+    created_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
+    updated_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
+    
+    
+    def __repr__(self):
+        return f"<user {self.username}>"

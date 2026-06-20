3125b184f0742fb5a2b8784eb25e8fe28d341ff9
vishal1011001 <vishaljakharbusinessmail@gmail.com>
2026-06-20 15:50:06 +0530

    created utils functions for generating and verifying hashed version of password

---
+++ src/auth/utils.py
@@ -0,0 +1,13 @@
+from passlib.context import CryptContext
+
+passwd_context = CryptContext(
+    schemes=['bcrypt']
+)
+
+def generate_passwd_hash(password: str) -> str:
+    hash = passwd_context.hash(password)
+    return hash
+
+def verify_passwd(password: str, hash: str) -> bool:
+    return passwd_context.verify(password, hash)
+

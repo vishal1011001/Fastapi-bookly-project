06a8c7cf2f60a7af5a9d021b0e293f8d04c0b147
vishal1011001 <vishaljakharbusinessmail@gmail.com>
2026-07-03 12:15:07 +0530

    Added Logout route to revoke token using Redis. Also implemented route for getting new access token using refresh token

--- src/auth/routes.py
+++ src/auth/routes.py
@@ -5,8 +5,10 @@ from sqlmodel.ext.asyncio.session import AsyncSession
 from fastapi.exceptions import HTTPException
 from src.db.main import get_session
 from .utils import verify_passwd, create_access_token, decode_token
-from datetime import timedelta
+from datetime import timedelta, datetime
 from fastapi.responses import JSONResponse
+from .dependencies import RefreshTokenBearer, AccessTokenBearer
+from src.db.redis import add_jti_to_blocklist
 
 auth_router = APIRouter()
 user_service = UserService()
@@ -14,7 +16,10 @@ user_service = UserService()
 REFRESH_TOKEN_EXPIRY_DAYS = 2
 
 @auth_router.post('/signup', status_code=status.HTTP_201_CREATED, response_model=UserResponse)
-async def create_user_account(user_data: UserCreateModel, session: AsyncSession = Depends(get_session)):
+async def create_user_account(
+    user_data: UserCreateModel, 
+    session: AsyncSession = Depends(get_session)
+):
     email = user_data.email
     
     user_exists = await user_service.user_exists(email, session)
@@ -64,3 +69,33 @@ async def user_signin(user_credentials: UserCredentials, session: AsyncSession =
             )
     else:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid email or password.")
+ 
+   
+@auth_router.get('/refresh_token')
+async def get_new_access_token(token_details: dict = Depends(RefreshTokenBearer())):
+    """Get new access token using valid refresh token"""
+    
+    expiry_timestamp = token_details['exp']
+    if datetime.fromtimestamp(expiry_timestamp) > datetime.now():
+        new_access_token = create_access_token(
+            user_data=token_details['user']
+        )
+        
+        return JSONResponse(content={
+            'access_token': new_access_token
+        })
+    
+    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, 
+                        detail="Invalid or expired token.")
+    
+@auth_router.post('/logout')
+async def revoke_token(token_details: dict = Depends(AccessTokenBearer())):
+    jti = token_details['jti']
+    await add_jti_to_blocklist(jti)
+    
+    return JSONResponse(
+        content={
+            'message': 'Logged out successfully.'
+        },
+        status_code=status.HTTP_200_OK
+    )
